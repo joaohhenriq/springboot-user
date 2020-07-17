59ec3d8624bc7b5da5179c5d05dbe52b9ccab2e6
joaoh <joao_camargo10@hotmail.com>
2020-07-17 15:50:01 -0300

    Lambdas

--- src/main/java/br/com/user/teste/Teste.java
+++ src/main/java/br/com/user/teste/Teste.java
@@ -2,6 +2,7 @@ package br.com.user.teste;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 import br.com.user.models.Cliente;
 
@@ -12,16 +13,40 @@ public class Teste {
 		Cliente c1 = new Cliente("joao", true, "123");
 		Cliente c2 = new Cliente("maria", true, "123");
 		c1.autenticaSenha("1234");
-		
+
 		List<Cliente> clientes = Arrays.asList(c1, c2);
-		
+
 //		for(Cliente cli : clientes) {
 //			System.out.println(cli.getNome());
 //			System.out.println(cli.getSenha());
 //		}
+
+//		MostraCliente mc = new MostraCliente();
+
+//		Consumer<Cliente> consumidor = new Consumer<Cliente>() {
+//
+//			@Override
+//			public void accept(Cliente t) {
+//				System.out.println(t.getNome());
+//			}
+//		};
 		
-		MostraCliente mc = new MostraCliente();
-		clientes.forEach(mc);
+		// como é uma interface funcional, ou seja, só tem um método, o compilador já sabe que deve inferir 
+		// essa função ao método accept
+		Consumer<Cliente> consumidor = (Cliente c) -> {System.out.println(c.getNome());};
+
+		clientes.forEach(consumidor);
+		
+		// ou pode fazer desse jeito, ainda mais simplificado e direto
+		clientes.forEach(c -> System.out.println(c.getNome()));
+		
+		//apenas dá pra utilizar uma expressão lambda quando é implementado uma interface funcional
+		
+		//-------------------------------------
+		
+		// Runnable é uma interface funcional, possui apenas o método run sem parâmetros
+		Runnable r = () -> System.out.println("Olá mundo");
+		new Thread(r).start();
 	}
 
 }
